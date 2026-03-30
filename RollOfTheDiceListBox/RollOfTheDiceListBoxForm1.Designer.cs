7720c9ded20aacc1f3d02cf57355f4e5ef17adfe
Hunter Clezie <clezhunt@gmail.com>
2026-03-30 08:40:14 -0600

    add header and named form

--- RollOfTheDiceListBox/RollOfTheDiceListBoxForm1.Designer.cs
+++ RollOfTheDiceListBox/RollOfTheDiceListBoxForm1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RollOfTheDiceListBox
 {
-    partial class Form1
+    partial class RollOfTheDiceListBoxForm1
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            SuspendLayout();
+            // 
+            // RollOfTheDiceListBoxForm1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Name = "RollOfTheDiceListBoxForm1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
---
+++ RollOfTheDiceListBox/RollOfTheDiceListBoxForm1.cs
@@ -0,0 +1,17 @@
+/* 
+Hunter Clezie 
+Spring 2026
+RCET2265
+RollOfTheDiceListBox
+github url: https://github.com/hunterclezie-pixel/RollOfTheDiceListBox.git
+*/
+namespace RollOfTheDiceListBox
+{
+    public partial class RollOfTheDiceListBoxForm1 : Form
+    {
+        public RollOfTheDiceListBoxForm1()
+        {
+            InitializeComponent();
+        }
+    }
+}
