4dc5de8f53150a56f4decd8b1d711d49d3b62b69
Hunter Clezie <clezhunt@gmail.com>
2026-03-30 11:26:00 -0600

    start progress on roll dice programming

--- RollOfTheDiceListBox/RollOfTheDiceListBoxForm1.Designer.cs
+++ RollOfTheDiceListBox/RollOfTheDiceListBoxForm1.Designer.cs
@@ -28,18 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            RollOfTheDiceLabel = new Label();
+            RollButton = new Button();
+            ClearButton = new Button();
+            ExitButton = new Button();
             SuspendLayout();
+            // 
+            // RollOfTheDiceLabel
+            // 
+            RollOfTheDiceLabel.AutoSize = true;
+            RollOfTheDiceLabel.Location = new Point(321, 78);
+            RollOfTheDiceLabel.Name = "RollOfTheDiceLabel";
+            RollOfTheDiceLabel.Size = new Size(117, 20);
+            RollOfTheDiceLabel.TabIndex = 0;
+            RollOfTheDiceLabel.Text = "Roll Of The Dice";
+            // 
+            // RollButton
+            // 
+            RollButton.Location = new Point(383, 378);
+            RollButton.Name = "RollButton";
+            RollButton.Size = new Size(131, 60);
+            RollButton.TabIndex = 1;
+            RollButton.Text = "Roll";
+            RollButton.UseVisualStyleBackColor = true;
+            // 
+            // ClearButton
+            // 
+            ClearButton.Location = new Point(520, 378);
+            ClearButton.Name = "ClearButton";
+            ClearButton.Size = new Size(131, 60);
+            ClearButton.TabIndex = 2;
+            ClearButton.Text = "Clear";
+            ClearButton.UseVisualStyleBackColor = true;
+            // 
+            // ExitButton
+            // 
+            ExitButton.Location = new Point(657, 378);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(131, 60);
+            ExitButton.TabIndex = 3;
+            ExitButton.Text = "Exit";
+            ExitButton.UseVisualStyleBackColor = true;
             // 
             // RollOfTheDiceListBoxForm1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(ExitButton);
+            Controls.Add(ClearButton);
+            Controls.Add(RollButton);
+            Controls.Add(RollOfTheDiceLabel);
             Name = "RollOfTheDiceListBoxForm1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label RollOfTheDiceLabel;
+        private Button RollButton;
+        private Button ClearButton;
+        private Button ExitButton;
     }
 }
--- RollOfTheDiceListBox/RollOfTheDiceListBoxForm1.cs
+++ RollOfTheDiceListBox/RollOfTheDiceListBoxForm1.cs
@@ -13,5 +13,47 @@ namespace RollOfTheDiceListBox
         {
             InitializeComponent();
         }
+
+        //Custom Methids below here --------------------------------------------------------------
+
+        void SetDefaults()
+        {
+            RollOfTheDiceLabel = RollDice;
+        }
+
+        void RollDice()
+        {
+            Random rand = new Random(); //Constructor
+            int[] rolls = new int[13]; // Array to count occurrences of each possible roll (2-12)
+            string columnSeperator = "|"; // String to separate columns in the output
+
+            Console.WriteLine("Roll Of The Dice");
+
+            for (int i = 0; i < 1000; i++)
+            {
+                int n = rand.Next(2, 12 + 1); // Random number between 2 and 12
+                rolls[n]++; // Increment the count for this roll
+            }
+
+            // Print the results:
+            // First line is the numbers 2-12
+            for (int i = 2; i <= 12; i++)
+            {
+                Console.Write($"{i}".PadRight(4) + columnSeperator);
+            }
+            Console.WriteLine();
+
+            // Second line is the number of times each number was rolled
+            for (int i = 2; i <= 12; i++)
+            {
+                Console.Write($"{rolls[i]}".PadRight(4) + columnSeperator);
+            }
+            Console.WriteLine();
+
+            //pause
+            Console.Read();
+        }
+
+        //Event Handlers below here --------------------------------------------------------------
     }
 }
